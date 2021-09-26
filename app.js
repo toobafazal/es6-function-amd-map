2723e8310504912ec18dc0b2efc65e05749e7bb0
toobafazal12 <tooba.fazal@asinlabs.tech>
2021-09-25 20:39:50 -0700

    es6

---
+++ app.js
@@ -0,0 +1,55 @@
+//write a program which shows the couter after each second.
+
+// function clock() {
+//    //shows time
+//    var d = new Date();
+//    var hours = d.getHours();
+//    var min = d.getMinutes();
+//    var sec = d.getSeconds();
+
+//    console.log(`Tooba = ${hours}:${min}:${sec}`)
+
+// }
+// setInterval(clock, 1000);
+
+
+
+
+// write a function which takes your name and displays the greeting with your name
+ // function doStuff(){
+//    console.log(`tooba`)
+//   
+// } 
+// console.log(num)
+
+// write a function(arrow function) which takes two values and return its sum as a result
+
+//  myFunc = () =>   6+7; 
+// let b = myFunc();
+// console.log(b)
+
+//write a function(arrow function) which takes a number and multiply it with 0.5 and return the new value.
+//  print the new value outside the function
+
+// let c = +prompt("enter your value")
+//  myFunc = () =>   0.5; 
+// let b = myFunc();
+// let a = c*b;
+// console.log(a);
+
+//print simple array of [1,2,3,4,5] with the help of array map funtion
+// let records = [1,2,3,4,5];
+// records.map((a)=>{
+//     if(records == 3){
+//       console.log(a)
+//     }
+//  })
+
+ // let arr = [{id:1,name:"abc"},{id:1,name:"efg"},{id:2,name:"hij"},{id:3,name:"xyz"}]
+//  arr = [{id:1,name:"abc"},{id:2,name:"efg"},{id:2,name:"hij"},{id:3,name:"xyz"}];
+//  arr.map((a)=>{
+//     if(a.name == "abc"){
+//       console.log(a.name)
+//     }
+//  })
+
